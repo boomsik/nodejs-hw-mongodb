5d36af03b81762f99422aab240cd5c03c90afaa6
boomsik <pozdnyakov151994@gmail.com>
2024-10-09 22:23:56 +0300

    add fix bugs

--- src/server.js
+++ src/server.js
@@ -1,37 +1,37 @@
-// const express = require('express');
-// const cors = require('cors');
-// const pino = require('pino-http')();
-// const cookieParser = require('cookie-parser');
-// const contactsRouter = require('./routes/contacts');
-// const authRouter = require('./routes/auth');
-// const errorHandler = require('./middlewares/errorHandler');
-// const notFoundHandler = require('./middlewares/notFoundHandler');
+const express = require('express');
+const cors = require('cors');
+const pino = require('pino-http')();
+const cookieParser = require('cookie-parser');
+const contactsRouter = require('./routes/contacts');
+const authRouter = require('./routes/auth');
+const errorHandler = require('./middlewares/errorHandler');
+const notFoundHandler = require('./middlewares/notFoundHandler');
 
-// async function setupServer() {
-//   const app = express();
+async function setupServer() {
+  const app = express();
 
-//   app.disable('etag');
+  app.disable('etag');
 
-//   app.use(cors());
-//   app.use(pino);
-//   app.use(express.json());
-//   app.use(cookieParser());
+  app.use(cors());
+  app.use(pino);
+  app.use(express.json());
+  app.use(cookieParser());
 
-//   app.use((req, res, next) => {
-//     res.set('Cache-Control', 'no-store');
-//     next();
-//   });
+  app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store');
+    next();
+  });
 
-//   app.use('/contacts', contactsRouter);
-//   app.use('/auth', authRouter);
+  app.use('/contacts', contactsRouter);
+  app.use('/auth', authRouter);
 
-//   app.use(notFoundHandler);
-//   app.use(errorHandler);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-// }
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-// module.exports = { setupServer };
+module.exports = { setupServer };
